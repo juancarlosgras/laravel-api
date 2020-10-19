0cbc61e846cb1c280995ee555c8e27d23ad5c4b4
JuanCarlos <juancarlosgras@gmail.com>
2020-10-19 17:30:17 -0400

    reversing test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/bug');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
